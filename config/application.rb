d6044182101a7dd5f35d271f03f3bc23cef73a9b
bauer-hillary-c <hbauer@ezcater.com>
2017-06-03 11:43:42 -0400

    generates user model

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  end
 
-  config.assets.initialize_on_precompile = false
+    # Required for Heroku
+    config.assets.initialize_on_precompile = false
+  end
 end
