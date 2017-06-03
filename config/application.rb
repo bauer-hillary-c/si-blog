6116c1644d20b8578f59ef7e179709d900bf4de4
bauer-hillary-c <hbauer@ezcater.com>
2017-06-03 11:40:30 -0400

    whoopsie

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module Blog
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
-end
 
-config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = false
+end
