e4cfe2a3d3b3658112babcd95a504877702d46e7
bauer-hillary-c <hbauer@ezcater.com>
2017-06-17 11:01:26 -0400

    creates spec for articles_controller

---
+++ spec/controllers/articles_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe ArticlesController, type: :controller do
+  describe "anonymous user" do
+    before :each do
+      login_with nil
+    end
+
+    it "should be redirected when trying to make a new post" do
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it "should let an anonymous user see the index" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "logged in user" do
+    before :each do
+      login_with create(:user)
+    end
+
+    it "should let a logged in user write a post"
+    get :new
+    expect(response).to render_template(:new)
+  end
+end
