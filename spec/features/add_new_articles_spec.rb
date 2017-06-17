06b7d6647748ee13e01a25ae89c4f5b19d672e77
bauer-hillary-c <hbauer@ezcater.com>
2017-06-17 11:25:38 -0400

    adds feature spec (broken right now, but will revisit)

---
+++ spec/features/add_new_articles_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSepc.feature "AddNewArticles", type: :feature do
+  it "should require the user to log in before adding an article" do
+    password = "123456789"
+    user = create(:user, password: password, password_confirmation: password)
+
+    visit new_article_path
+
+    within "#new_user" do
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: password
+    end
+
+    click_button "Log in"
+
+    within "#new_article" do
+      fill_in 'article_title', with: "Article Title"
+      fill_in 'article_text', with: "Article body"
+    end
+
+    click_button "Save Article"
+
+    expect(Article.count).to eq(1)
+    expect(Article.first.title).to eq("Article Title")
+  end
+end
